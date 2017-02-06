3ff000ac912b14f320f20ccbb9b1da1e57f03bcb
Jon Hall <jon@flowmo.co>
2017-02-06 09:34:14 +0000

    Inject dependencies into zendesk client

--- src/Controller/ZendeskConnectController.php
+++ src/Controller/ZendeskConnectController.php
@@ -1,91 +1,100 @@
 <?php
 
 namespace Drupal\zendesk_connect\Controller;
+
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\zendesk_connect\Http\ZendeskConnectHttp;
+use Drupal\zendesk_connect\Http\ZendeskClient;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ZendeskConnectController extends ControllerBase {
 
-	// all requests
-	public function requests() {
-		return [
+  /**
+   * @var \Drupal\zendesk_connect\Http\ZendeskClient
+   */
+  private $zendeskClient;
+
+  public function __construct(ZendeskClient $zendeskClient) {
+    $this->zendeskClient = $zendeskClient;
+  }
+
+  public function requests() {
+    return [
       '#theme' => 'requests',
       '#title' => 'Requests page',
-			'#requests' => $this->getAllReq(),
-			'#attached' => [
+      '#requests' => $this->getAllReq(),
+      '#attached' => [
         'library' => [
           'zendesk_connect/requests-styles',
-        ]
-      ]
+        ],
+      ],
     ];
-	}
+  }
 
-	// single request
-	public function request($id) {
-		return [
+  public function request($id) {
+    return [
       '#theme' => 'request',
-			'#title' => $this->getReq($id)->request->subject,
+      '#title' => $this->getReq($id)->request->subject,
       '#request_id' => $id,
-			'#request' => $this->getReq($id),
-			'#comments' => $this->getReqCom($id),
-			'#attached' => [
+      '#request' => $this->getReq($id),
+      '#comments' => $this->getReqCom($id),
+      '#attached' => [
         'library' => [
           'zendesk_connect/requests-styles',
-        ]
-      ]
+        ],
+      ],
     ];
-	}
+  }
 
-	// new request
-	public function newRequest() {
-		return [
-			'#theme' => 'new_request',
-			'#title' => 'New Request',
-		];
-	}
+  public function newRequest() {
+    return [
+      '#theme' => 'new_request',
+      '#title' => 'New Request',
+    ];
+  }
 
-	public function getAllReq() {
-		$endpoint = '/api/v2/requests.json';
-	  $check = new ZendeskConnectHttp();
-	  $response = $check->performRequest($endpoint);
-		return $response;
-	}
+  public function getAllReq() {
+    return $this->zendeskClient->performRequest('/api/v2/requests.json');
+  }
 
-	public function getReq($id) {
-		$endpoint = '/api/v2/requests/' . $id . '.json';
-	  $check = new ZendeskConnectHttp();
-	  $response = $check->performRequest($endpoint);
-		return $response;
-	}
+  public function getReq($id) {
+    return $this->zendeskClient->performRequest("/api/v2/requests/{$id}.json");
+  }
 
-	public function getReqCom($id) {
-		$endpoint = '/api/v2/requests/' . $id . '/comments.json';
-	  $check = new ZendeskConnectHttp();
-	  $response = $check->performRequest($endpoint);
-		return $response;
-	}
+  public function getReqCom($id) {
+    return $this->zendeskClient->performRequest("/api/v2/requests/{$id}/comments.json");
+  }
 
-	public function userData() {
-		if ($_SESSION['auth0__user_info']) {
-			return json_encode($_SESSION['auth0__user_info']);
-		} else {
-			return NULL;
-		}
-	}
+  public function userData() {
+    if ($_SESSION['auth0__user_info']) {
+      return json_encode($_SESSION['auth0__user_info']);
+    }
+    else {
+      return NULL;
+    }
+  }
 
-	public function accessToken() {
-		if ($_SESSION['auth0__access_token']) {
-			return $_SESSION['auth0__access_token'];
-		} else {
-			return NULL;
-		}
-	}
+  public function accessToken() {
+    if ($_SESSION['auth0__access_token']) {
+      return $_SESSION['auth0__access_token'];
+    }
+    else {
+      return NULL;
+    }
+  }
 
-	public function idToken() {
-		if ($_SESSION['auth0__id_token']) {
-			return $_SESSION['auth0__id_token'];
-		} else {
-			return NULL;
-		}
-	}
+  public function idToken() {
+    if ($_SESSION['auth0__id_token']) {
+      return $_SESSION['auth0__id_token'];
+    }
+    else {
+      return NULL;
+    }
+  }
+
+  public static function create(ContainerInterface $container) {
+    $zendeskClient = $container->get('zendesk_connect.client');
+
+    return new static($zendeskClient);
+  }
+
 }
---
+++ src/Http/ZendeskClient.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\zendesk_connect\Http;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class ZendeskClient {
+
+  use StringTranslationTrait;
+
+  /**
+   * @var \GuzzleHttp\Client
+   */
+  private $client;
+
+  /**
+   * @var string
+   */
+  private $domain;
+
+  /**
+   * @var string
+   */
+  private $token;
+
+  /**
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  private $account;
+
+  public function __construct(
+    Client $client,
+    ConfigFactoryInterface $configFactory,
+    AccountProxyInterface $account
+  ) {
+    $this->client = $client;
+    // @todo Pass in config values as own parameters
+    $config = $configFactory->get('zendesk_connect.settings');
+    $this->domain = $config->get('zendesk_domain');
+    $this->token = $config->get('zendesk_api_token');
+    $this->account = $account;
+  }
+
+  public function performRequest($endpoint) {
+    $url = $this->domain . $endpoint;
+    $email = $this->account->getEmail();
+
+    try {
+      $result = $this->client->get(
+        $url,
+        ['http_errors' => FALSE, 'auth' => [$email, $this->token]]
+      );
+      return (json_decode($result->getBody()));
+    } catch (RequestException $e) {
+      return $this->t('Error');
+    }
+  }
+
+}
